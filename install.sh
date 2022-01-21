0a5f52f7b4edcf7ed9c9a38f5069ef0a44462ac4
Krstan Vjestica <krle@tilda.center>
2022-01-21 16:37:26 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -Sy neovim python3-pip keepassxc vagrant docker curl wget unp unzip unrar firefox dino geeqie fish mpv audacious bpytop pulseaudio protonvpn
+sudo install_pulse
+sudo systemctl --user start pulseaudio 
+
+bash <(curl -sL https://raw.githubusercontent.com/krlex/nodejs-installation/main/script.sh) tls 
+
